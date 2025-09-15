e58a378c3aadb26606d04b8476bcb7275dd7420a
Infant Luvango P <infant.l@disprz.com>
2025-09-15 15:03:43 +0530

    sample commit

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddScoped<IEventsRepo, EventsRepo>();
 
 builder.Services.AddScoped<IRecurrence, Recurrence>();
 
-
+// sample
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
